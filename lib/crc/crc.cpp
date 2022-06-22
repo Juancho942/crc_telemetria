8f095db54430bebee9effb20de27830dd0e9cbbb
Juan David <juandavidbetancurvalencia>
2022-06-21 23:10:38 -0500

    cambios calculo crc

--- lib/crc/crc.cpp
+++ lib/crc/crc.cpp
@@ -3,12 +3,9 @@
 
 crc::crc(byte numeroCrc)
 {
-    Serial.println(int(numeroCrc));
-    Serial.println('\n');
     for (int i = 0; i < 7; i++)
     {
         _comparacionXor[i] = bitRead(numeroCrc, i);
-        // Serial.print(int(_comparacionXor[i]));
     }
 }
 byte crc::calculoCrc(byte vector[20])
@@ -20,19 +17,20 @@ byte crc::calculoCrc(byte vector[20])
 
     byte calculo_crc[136];
     byte G[7] = {1, 0, 0, 1, 1, 0, 1};
-    byte temp[7];
     byte vectorTemporal[7];
-    int bandera = 0;
-    int z, c, a = 0;
+    int a = 0;
+    int caso = 0;
+    int cerosIncrementar = 7;
+    boolean cicloGeneral;
     memset(calculo_crc, 3, 136);
     for (int i = 1; i < 18; i++)
     { // empiezo a recorrer byte por byte de la cadena de datos
-        Serial.print(i);
-        Serial.println(" for para convertir los binarios de vector con i : ");
+        // Serial.print(i);
+        // Serial.println(" for para convertir los binarios de vector con i : ");
         int b = 0;
         for (int j = 0; j < 136; j++)
         {
-            Serial.println("calculando posicion vector");
+            // Serial.println("calculando posicion vector");
             if (calculo_crc[j] != 3)
             {
                 b += 1;
@@ -40,63 +38,61 @@ byte crc::calculoCrc(byte vector[20])
             else
                 break;
         }
-        Serial.print("b: ");
-        Serial.println(b);
         a = 7;
         for (int j = b; j < (b + 8); j++)
         {
-            Serial.print("for que saca los binarios de cada byte con j : ");
-            Serial.println(j);
+            // Serial.print("for que saca los binarios de cada byte con j : ");
+            // Serial.println(j);
             calculo_crc[j] = bitRead(_vector[i], a);
             a -= 1;
         }
-        for (int i = 0; i < 136; i++)
-        {
-            Serial.print(calculo_crc[i]);
-        }
-        Serial.println();
     }
-    boolean variable = true;
-    while (variable)
+    for (int i = 0; i < 136; i++)
     {
-
-        for (int z = 0; z < 7; z++)
-        { // copio los binarliios que se bajan del vector principal
-            Serial.println("copio los binarios ");
-            vectorTemporal[z] = calculo_crc[c];
-            c += 1;
-        }
-        for (int i = 0; i < 7; i++)
-        { // realiza la xor
-            temp[i] = G[i] ^ vectorTemporal[i];
-            //Serial.println("realiza la operacion xor");
-            Serial.print(temp[i]);
-        }
-        z = 0;
-        while (temp[z] == 0)
-        { // cuento de ceros
-            Serial.println("ingreso while que me cuenta los ceros a mover");
-            bandera += 1; // incremento la variable el numero de ceros q se va contanto
-            z += 1;
-        }
-        if (bandera != 0)
-        { // verifico si hubo conteo de ceros y realizo los respectivos cambios
-            Serial.println("ingreso if verificar cuantos ceros a mover");
-            for (int i = 0; i < (7 - bandera); i++)
+        vectorTemporal[7 - cerosIncrementar] = calculo_crc[i];
+        cerosIncrementar -= 1;
+        cicloGeneral = true;
+        if (cerosIncrementar == 0)
+        {
+            Serial.println();
+            while (cicloGeneral)
             {
-                Serial.println("ingreso for");
-                vectorTemporal[i] = temp[z]; // elimino de la variable los ceros empezando a copiarla de a donde se encontro el primer uno
-                z += 1;
-                variable = false;
+                if (caso == 0)
+                {
+                    int j = 0;
+                    while (vectorTemporal[j] == 0)
+                    {
+                        cerosIncrementar += 1;
+                        j += 1;
+                    }
+                    if (cerosIncrementar > 0)
+                    {
+                        for (int b = 0; b < 7 - cerosIncrementar; b++)
+                        {
+                            vectorTemporal[b] = vectorTemporal[b + cerosIncrementar];
+                        }
+                        cicloGeneral = false;
+                    }
+                    caso = 1;
+                }
+
+                else if (caso == 1)
+                {
+                    for (int c = 0; c < 7; c++)
+                    { // realiza la xor
+                        vectorTemporal[c] = G[c] ^ vectorTemporal[c];
+                        // Serial.println("realiza la operacion xor");
+                        Serial.print(vectorTemporal[c]);
+                    }
+                    caso = 0;
+                }
             }
-            bandera = 0;
         }
     }
-
     byte retorno;
     for (int i = 0; i < 7; i++)
     {
-        bitWrite(retorno, temp[i], (6 - i));
+        bitWrite(retorno, vectorTemporal[i], (6 - i));
     }
     return (retorno);
 }
